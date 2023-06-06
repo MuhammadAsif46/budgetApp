b0509e3b28e4f83f9bb470370260bcd84b29aa1b
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-06-06 23:00:31 +0500

    Update app.js with Get Variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// Get All Variables :
+ 
+let balance = document.getElementById("balance");
+let totalAmount = document.getElementById("total-amount");
+let totalBudget = document.getElementById("total-budget");
+let description = document.getElementById("description");
+let inputAmount = document.getElementById("input-amount");
+let inputDate = document.getElementById("input-date");
+let expence = document.getElementById("expence");
+let expenceList = document.getElementById("add-expence-list");
+let checkButton = document.getElementById("check-button");
